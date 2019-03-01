8344827ca97d370aad362848be33f09fcf606b05
KieranVieira <kieranvieira@live.com>
2019-03-01 11:32:04 -0500

    Added register and login routes using jwt and bcrypt

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+const db = require('../database/dbConfig');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -8,12 +11,63 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+const secret = "some interesting secret hmmmm..."
+
+function getToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secret, options)
+}
+
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+  db('users')
+    .insert(user)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Server could not register user",
+        error
+      })
+    })
 }
 
 function login(req, res) {
-  // implement user login
+  const {username, password} = req.body;
+  
+  db('users')
+    .where({ username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)){
+        const token = getToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        })
+      }else{
+        res.status(400).json({
+          message:"Invalid credentials"
+        })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message:"Server could not login user",
+        error
+      })
+    })
+  
 }
 
 function getJokes(req, res) {
